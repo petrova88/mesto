c1e4da5a059b74a69cd016c559c09dc2f4951c98
mariapetrova0811@yandex.ru <mariapetrova0811@gmail.com>
2023-04-03 17:34:06 +0300

    файлы по БЭМ

---
+++ script.js
@@ -0,0 +1,10 @@
+const openPopupBtn = document.querySelector('.popup_opened');
+const popup = document.querySelector('.popup');
+
+function openPopup() {
+    popup.classList.add('.popup_opened');
+}
+
+function closePopup(evt) {
+    const 
+}
