70d84955c65736b9f68e4ed3be45295773de91e1
mariapetrova0811@yandex.ru <mariapetrova0811@gmail.com>
2023-04-08 15:28:08 +0300

    доделываю JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,28 @@
-const popup = document.querySelector('.popup');
-const editPopup = document.querySelector('.edit-button');
-const popupCloseBtn = document.querySelector('.popup__close-btn');
-const nameInput = document.querySelector('.popup__input_name');
-const jobInput = document.querySelector('.popup__input_job');
-const nameInProfile = document.querySelector('.profile__title');
-const jobInProfile = document.querySelector('.profile__subtitle');
-const popupInput = document.querySelector('popup__input-container');
-const saveBtn = document.querySelector('.popup__submit-btn');
+const popup = document.querySelector('.popup'); // +
+const editPopup = document.querySelector('.edit-button'); // +
+const popupCloseBtn = document.querySelector('.popup__close-btn'); // +
+const nameInput = document.querySelector('.popup__input_name'); // +
+const jobInput = document.querySelector('.popup__input_job'); // +
+const nameInProfile = document.querySelector('.profile__title'); // +
+const jobInProfile = document.querySelector('.profile__subtitle'); // +
+const formElement = document.querySelector('popup__input-container'); // +
+// const saveBtn = document.querySelector('.popup__submit-btn'); // -
 
 // открыть попап
 function openPopup() {
-    popup.classList.add ('popup_opened');
+    popup.classList.add('popup_opened');
     nameInput.value =  nameInProfile.textContent;
     jobInput.value = jobInProfile.textContent;
 }
 
 // закрыть попап
 function closePopup() {
-    popup.classList.remove ('popup_opened');
+    popup.classList.remove('popup_opened');
 } 
 
 // сохранить данные из input
 
-function handleFormSubmit (evt) {
+function handleFormSubmit(evt) {
     evt.preventDefault();
     nameInProfile.textContent = nameInput.value;
     jobInProfile.textContent = jobInput.value;
@@ -31,13 +31,6 @@ function handleFormSubmit (evt) {
 
 // сохранить изменения
 
-function closeSavePopup () {
-    popup.classList.remove ('popup_opened');
-    nameInProfile.textContent = nameInput.value;
-    jobInProfile.textContent = jobInput.value;
-}
-
 editPopup.addEventListener('click', openPopup);
 popupCloseBtn.addEventListener('click', closePopup);
-saveBtn.addEventListener('click', closeSavePopup);
-
+formElement.addEventListener('submit', handleFormSubmit);
