8a2aa3edf6424908d6af16ed92bf7d9f424ce34d
mariapetrova0811@yandex.ru <mariapetrova0811@gmail.com>
2023-04-15 16:05:01 +0300

    добавила попап редактирования и возможность ставить лайки

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,13 @@ const jobInput = document.querySelector('.popup__input_job');
 const nameInProfile = document.querySelector('.profile__title');
 const jobInProfile = document.querySelector('.profile__subtitle');
 const formElement = document.querySelector('.popup__input-container');
+const popupMesto = document.querySelector('.popup-mesto');
+const addButton = document.querySelector('.add-button');
+const mestoCloseBtn = document.querySelector('.popup-mesto__close-btn');
+const likes = document.querySelectorAll('.photo__like');
 
-// открыть попап
+
+// попап редактирования профиля
 function openPopup() {
     popup.classList.add('popup_opened');
     nameInput.value =  nameInProfile.textContent;
@@ -19,7 +24,7 @@ function closePopup() {
     popup.classList.remove('popup_opened');
 } 
 
-// сохранить данные из input
+// сохранить данные из input в ред профиля
 function handleFormSubmit(evt) {
     evt.preventDefault();
     nameInProfile.textContent = nameInput.value;
@@ -27,7 +32,57 @@ function handleFormSubmit(evt) {
     closePopup();
 }
 
-// сохранить изменения
+// попап добавления
+function openMesto() {
+    popupMesto.classList.add('popup-mesto_opened');
+}
+
+function closeMesto() {
+    popupMesto.classList.remove('popup-mesto_opened');
+}
+
+// попап редактирования
 editPopup.addEventListener('click', openPopup);
 popupCloseBtn.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
+
+// попап добавления
+addButton.addEventListener('click', openMesto);
+mestoCloseBtn.addEventListener('click', closeMesto);
+
+// поставить лайк
+likes.forEach((item) => {
+    item.addEventListener('click', () =>
+    item.classList.toggle('photo__like_active'));
+});
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  
+
+
